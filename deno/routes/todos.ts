5f431638c9da83057f1036b6ec19d2559c9f6e93
huynhquynh-dev <huynhquynh-dev>
2021-09-09 15:43:55 +0700

    04-store-and-fetch

--- deno/routes/todos.ts
+++ deno/routes/todos.ts
@@ -1,28 +1,37 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
+import { ObjectId } from 'https://deno.land/x/mongo@v0.8.0/mod.ts';
 
 import { getDb } from '../helpers/db_client.ts';
 
 const router = new Router();
 
 interface Todo {
-  id: string;
+  id?: string;
   text: string;
 }
 
 let todos: Todo[] = [];
 
-router.get('/todos', (ctx) => {
-  ctx.response.body = { todos: todos };
+router.get('/todos', async (ctx) => {
+  const todos = await getDb().collection('todos').find(); // { _id: ObjectId(), text: '...' }[]
+  const transformedTodos = todos.map(
+    (todo: { _id: ObjectId; text: string }) => {
+      return { id: todo._id.$oid, text: todo.text };
+    }
+  );
+  ctx.response.body = { todos: transformedTodos };
 });
 
 router.post('/todos', async (ctx) => {
   const data = await ctx.request.body();
   const newTodo: Todo = {
-    id: new Date().toISOString(),
+    // id: new Date().toISOString(),
     text: data.value.text,
   };
 
-  todos.push(newTodo);
+  const id = await getDb().collection('todos').insertOne(newTodo);
+
+  newTodo.id = id.$oid;
 
   ctx.response.body = { message: 'Created todo!', todo: newTodo };
 });
