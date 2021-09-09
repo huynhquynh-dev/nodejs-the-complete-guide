784a9ed3c71446cc71976430979b47ffc8895032
huynhquynh-dev <huynhquynh-dev>
2021-09-09 15:43:31 +0700

    03-mongodb-helper

--- deno/routes/todos.ts
+++ deno/routes/todos.ts
@@ -1,37 +1,28 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
-import { ObjectId } from 'https://deno.land/x/mongo@v0.8.0/mod.ts';
 
 import { getDb } from '../helpers/db_client.ts';
 
 const router = new Router();
 
 interface Todo {
-  id?: string;
+  id: string;
   text: string;
 }
 
 let todos: Todo[] = [];
 
-router.get('/todos', async (ctx) => {
-  const todos = await getDb().collection('todos').find(); // { _id: ObjectId(), text: '...' }[]
-  const transformedTodos = todos.map(
-    (todo: { _id: ObjectId; text: string }) => {
-      return { id: todo._id.$oid, text: todo.text };
-    }
-  );
-  ctx.response.body = { todos: transformedTodos };
+router.get('/todos', (ctx) => {
+  ctx.response.body = { todos: todos };
 });
 
 router.post('/todos', async (ctx) => {
   const data = await ctx.request.body();
   const newTodo: Todo = {
-    // id: new Date().toISOString(),
+    id: new Date().toISOString(),
     text: data.value.text,
   };
 
-  const id = await getDb().collection('todos').insertOne(newTodo);
-
-  newTodo.id = id.$oid;
+  todos.push(newTodo);
 
   ctx.response.body = { message: 'Created todo!', todo: newTodo };
 });
